985dd47b5ccaf000b01b7e6715a5c6ab6d9852c1
Esther Okiki <estherokiki15@gmail.com>
2024-05-11 19:43:22 +0100

    Nav Bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 "use script;"
+
+// MENU BUTTON
+const menuBtn = document.getElementById('menuBtn');
+const menuBtnImg = document.querySelector('.nav--bar__menuBtn img');
+const navLink = document.querySelector('.nav--links')
+
+const btnClick = function () {
+
+    menuBtn.addEventListener('click', ()=>{
+        if (menuBtnImg.getAttribute('src') === './assets/img/menu-gray.png') {
+            menuBtnImg.setAttribute('src', './assets/img/close-gray.png');
+            navLink.classList.add('visible');
+
+      menuBtn.classList.toggle('rotate');
+      menuBtn.classList.remove('unRotate');
+
+
+ 
+
+        }else{
+            menuBtnImg.setAttribute('src', './assets/img/menu-gray.png');
+            navLink.classList.remove('visible');
+            navLink.style.transition = 'top 500ms ease';
+    
+
+      menuBtn.classList.toggle('unRotate');
+      menuBtn.classList.remove('rotate');
+
+        }
+    })
+}
+
+btnClick();
